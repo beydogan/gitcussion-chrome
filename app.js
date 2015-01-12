cca4a44eab60f98aedd7eb6902ef5c81bd252189
Mehmet Beydogan <mbbrain@gmail.com>
2015-01-12 23:14:39 +0200

    Add comments to Github repo page Add Templates

--- app.js
+++ app.js
@@ -1,10 +1,61 @@
+var apiBase = "https://gitcuss.herokuapp.com/";
+var repoName = $('meta[property="og:title"]').attr("content");
+var templatesUrl = chrome.extension.getURL('templates/');
+
+$(document).ready(function(){
+
+
+
+Handlebars.getTemplate = function(name) {
+    if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+        $.ajax({
+            url : templatesUrl + name + '.html',
+            datatype: 'text/javascript',
+            success : function(response, status, jqXHR) {
+                if (Handlebars.templates === undefined) {
+                    Handlebars.templates = {};
+                }
+                //Handlebars.templates[name] = Handlebars.compile(jqXHR.responseText);
+                Handlebars.templates[name] = Handlebars.compile(response);
+            },
+            async : false
+        });
+    }
+    return Handlebars.templates[name];
+};
+
+var addGitcussionBox = function (repo){
+	var template = Handlebars.getTemplate('gitcussion_box');
+	Handlebars.registerPartial("comment", Handlebars.getTemplate('comment'));
+	var context = {repo: repo};
+	var html    = template(context);
+	return html
+};
+
+var setBindings = function(){
+	$("#gitcussion").on("click", "h1", function(){
+		$("#gitcussion > article").slideToggle();
+		$(this).toggleClass("collapsed");
+		$(this).find("span").toggleClass("octicon-triangle-down");
+		$(this).find("span").toggleClass("octicon-triangle-up");
+	});
+}
+
+
 var xhr = new XMLHttpRequest();
 var resp;
-xhr.open("GET", "http://localhost:3000/r/ruby/ruby", true);
+xhr.open("GET", apiBase + "/r/" + repoName, true);
 xhr.setRequestHeader("Content-Type", "application/json");
 
 xhr.onload = function () {
     resp = JSON.parse(xhr.responseText);
     console.log(resp);
+    $("#readme").after(addGitcussionBox(resp));
+    setBindings();
 }
 xhr.send();
+
+	
+
+})
+
